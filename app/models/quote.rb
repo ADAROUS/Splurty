613f9c02dc8c9fcb3d3178995a033ccdab4ccec1
Diana Ambrosie <dianaambrosie@gmail.com>
2019-10-15 04:31:14 +0000

    added 3 quotes to heroku console

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,5 +1,4 @@
 class Quote < ApplicationRecord
-  # testing 1 2 3 
-  validates :saying, presence: true, length: { maximum: 140, minimum: 3 }
+  validates :saying, presence: true, length: { maximum: 1000, minimum: 3 }
   validates :author, presence: true, length: { maximum: 50, minimum: 3 }
 end
